e7301b7d8f08ee5263f3ffabb47393ee3e122165
kuroshi <helloThere@spencerjulian.com>
2013-04-30 17:47:28 -0400

    Possible fix for issue #2, still testing. Pushing to github to move code to my dev box.

--- script.xbmal/default.py
+++ script.xbmal/default.py
@@ -40,10 +40,10 @@ class ListGenerator():
 					continue #Don't do "season 0"
 				if self.config.showInConfig(season[0]['tvshowid'], season[0]['season']) == False:
 					try:
-						self.output.log("Listing Show: " + season[0]['showtitle'].encode('ascii', 'ignore'), xbmc.LOGNOTICE)
+						self.output.log("Listing Show: " + season[0]['showtitle'].__repr__().encode('ascii', 'ignore'), xbmc.LOGNOTICE)
 					except:
 						self.output.log("The problematic show appears to be the next one, alphebetically. Please send this log along with the name of that show to the developer via github and xbmclogs.com.", xbmc.LOGNOTICE)
-					searchResults = self.a.search(season[0]['showtitle'].encode('ascii', 'ignore'))
+					searchResults = self.a.search(season[0]['showtitle'].__repr__().encode('ascii', 'ignore'))
 					if (searchResults is False):
 						searchResult = [{'id':'%skip%', 'title':__settings__.getLocalizedString(411)}]
 					else:
